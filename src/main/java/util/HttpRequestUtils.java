35764ce69b6e78db86a1e27cfc4591e155d72541
KoTaeHyun <kotaehyun90@gmail.com>
2023-03-31 10:26:31 +0900

    index.html 출력

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -8,16 +8,8 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 
 public class HttpRequestUtils {
-    /**
-     * @param 
-     * @return
-     */
-    public static Map<String, String> mainSite(String inputInfo) {
-        return parseValues(inputInfo, " ");
-    }
-	
-	
-    /**
+
+	/**
      * @param queryString은
      *            URL에서 ? 이후에 전달되는 field1=value1&field2=value2 형식임
      * @return
